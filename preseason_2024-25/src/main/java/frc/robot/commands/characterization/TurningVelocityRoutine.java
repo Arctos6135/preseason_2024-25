d68a3576b25ba05cc7f5d6a2fc20a771ebf3c1eb
Micah Hewko <micah.hewko@student.tdsb.on.ca>
2024-07-04 14:22:59 -0400

    Implemented a turning velocity routine for the turning motors.

---
+++ preseason_2024-25/src/main/java/frc/robot/commands/characterization/TurningVelocityRoutine.java
@@ -0,0 +1,50 @@
+package frc.robot.commands.characterization;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+import frc.robot.subsystems.swerve.Swerve;
+
+public class TurningVelocityRoutine extends Command {
+    private final LoggedMechanismGroup mechanism;
+    private final double maxDistance;
+    private final double rampRate;
+    private double voltage = 0;
+    private double lastTimestamp;
+
+    public TurningVelocityRoutine(LoggedMechanismGroup mechanism, double maxDistance, double rampRate, Subsystem... requirements) {
+        this.mechanism = mechanism;
+        this.maxDistance = maxDistance;
+        this.rampRate = rampRate;
+
+        addRequirements(requirements);
+    }
+
+    @Override
+    public void initialize() {
+        Logger.recordOutput("Running", true);
+        lastTimestamp = Timer.getFPGATimestamp();
+    }
+
+    @Override
+    public void execute() {
+        Logger.recordOutput("Running at voltage", voltage);
+        System.out.printf("Running at voltage %f\n", voltage);
+        double timestamp = Timer.getFPGATimestamp();
+        voltage += rampRate * (timestamp - lastTimestamp);
+        lastTimestamp = timestamp;
+
+        mechanism.setVoltage(voltage);
+    }
+
+    @Override
+    public void end(boolean interupted) {
+        Logger.recordOutput("Running", false);
+
+        mechanism.setVoltage(0);
+    }
+}
